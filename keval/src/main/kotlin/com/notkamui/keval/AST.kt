0eb0bfda09d9687d0be2284df5d0848a5eef5697
notKamui <teillard.jimmy@outlook.fr>
2021-01-28 20:41:31 +0100

    Simple evaluable binary tree with binary operators

---
+++ keval/src/main/kotlin/com/notkamui/keval/AST.kt
@@ -0,0 +1,76 @@
+package com.notkamui.keval
+
+import kotlin.math.pow
+
+/**
+ * Represents a node in an AST and can evaluate its value
+ *
+ * Can either be an operator, or a leaf (a value)
+ */
+internal interface Node {
+    /**
+     * Evaluates the value of this node
+     *
+     * @return the value of the node
+     */
+    fun eval(): Double
+}
+
+/**
+ * An binary operator node
+ *
+ * @property left is its left child
+ * @property operator is the actual operator
+ * @property right is its right child
+ * @constructor Creates an operator node
+ */
+internal data class OperatorNode(
+    private val left: Node,
+    private val operator: Operator,
+    private val right: Node
+) : Node {
+    override fun eval(): Double = operator.apply(left.eval(), right.eval())
+}
+
+/**
+ * A value node (leaf)
+ *
+ * @property value is its value
+ * @constructor Creates a value node
+ */
+internal data class ValueNode(
+    private val value: Double
+) : Node {
+    override fun eval(): Double = value
+}
+
+/**
+ * Represents all operators
+ *
+ * @property symbol is the symbol of the operator
+ * @property precedence is the priority of the operator
+ * @property apply is the function linked to the operator
+ * @constructor Creates an Operator type
+ */
+internal enum class Operator(
+    val symbol: Char,
+    val precedence: Int,
+    val apply: (Double, Double) -> Double
+) {
+    ADD('+', 2, { a, b -> a + b }),
+    SUB('-', 2, { a, b -> a - b }),
+    MUL('*', 3, { a, b -> a * b }),
+    DIV('/', 3, { a, b -> a / b }),
+    MOD('%', 3, { a, b -> a % b }),
+    EXP('^', 4, { a, b -> a.pow(b) });
+
+    companion object {
+        /**
+         * Gets an operator by its symbol
+         *
+         * @param symbol is the symbol to get the corresponding operator
+         * @return the corresponding operator (or null)
+         */
+        operator fun get(symbol: Char): Operator? = values().firstOrNull { it.symbol == symbol }
+    }
+}
---
+++ keval/src/test/kotlin/com/notkamui/keval/ASTTest.kt
@@ -0,0 +1,24 @@
+package com.notkamui.keval
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class ASTTest {
+    @Test
+    fun simpleEvalTest() {
+        val ast: Node = OperatorNode(ValueNode(3.0), Operator.ADD, ValueNode(2.0))
+        assertEquals(ast.eval(), 5.0)
+    }
+
+    @Test
+    fun operatorGetterTest() {
+        val op = Operator['%']
+        val op2 = Operator['a']
+        val op3 = Operator['-']!!
+        assertEquals(op, Operator.MOD)
+        assertNull(op2)
+        assertTrue(op3.symbol == '-' && op3.precedence == 2 && op3.apply(5.0, 2.0) == 3.0)
+    }
+}
