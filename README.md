68dae1dc1fcfdf89f73abd647e6870f72e10e39f
notKamui <teillard.jimmy@outlook.fr>
2021-01-28 18:25:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Keval
+
+***A Kotlin mini library for string evaluation***
+
+(You may need to watch out using it: 
+having to evaluate a string into a number is more often than not a code smell)
---
+++ keval/build.gradle.kts
@@ -0,0 +1,40 @@
+/*
+ * @notKamui
+ *
+ * Gradle build file for Keval
+ */
+
+version = "BETA-0.1"
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.20"
+    `java-library`
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.guava:guava:29.0-jre")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    api("org.apache.commons:commons-math3:3.6.1")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+}
+
+java {
+    withSourcesJar()
+}
