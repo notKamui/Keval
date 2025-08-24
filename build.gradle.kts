0b7c6308fca6c8cedf7c156a945f159fd3a5b3e5
Finn Ebeling <46824694+finebel@users.noreply.github.com>
2025-08-24 10:06:09 +0200

    Add Apple targets based on https://kotlinlang.org/docs/native-target-support.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,19 @@ kotlin {
     linuxX64()
     mingwX64()
 
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    iosArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+
     sourceSets {
         val commonMain by getting {
         }
